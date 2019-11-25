b6ff60b1487f8eee4dbbb21bdf74f030f77583dc
Barbara Böszöri <eboszori@gmail.com>
2019-11-25 15:52:12 +0100

    SQLcomponent segédosztály a Node-hoz

---
+++ Model/NLHandler/SQLComponent.py
@@ -0,0 +1,21 @@
+# Az adott node-hoz kapcsolódó SQL komponenst tartalmazza
+
+class SQLComponent:
+
+    type = None
+    value = None
+    score = 1.0
+
+    def __init__(self, type, value, score=1.0):
+        self.type = type
+        self.value = value
+        self.score = score
+
+    def getType(self):
+        return self.type
+
+    def getScore(self):
+        return self.score
+
+    def getValue(self):
+        return self.value
