833add2bb939651dec4c5ec5b1420e55187f3d64
Barbara Böszöri <eboszori@gmail.com>
2019-11-30 20:18:33 +0100

    Word osztály

---
+++ Model/NLHandler/ParseTree.py
@@ -0,0 +1,12 @@
+from Model.NLHandler import Node
+from Model.NLHandler import Parser
+
+class ParseTree:
+    mod = None
+    root = None
+    nodelist = list()
+
+    def __init__(self):
+        self.mod = 0
+
+
--- Model/NLHandler/Parser.py
+++ Model/NLHandler/Parser.py
@@ -1,4 +1,5 @@
 import spacy
+from Model.NLHandler import ParseTree
 from spacy import displacy
 
 #class Parser:
@@ -8,17 +9,33 @@ userquestion = nlp('Spacy is my tool, and I am trying to use it.')
 #for sentence in userquestion.sents:
 #    print(sentence)
 
-tokens = [token.text for token in userquestion]
+tokens = [[token.text, token.pos_, token.tag_, token.dep_, token.head.text, token.lemma_] for token in userquestion]
 
-for word  in userquestion:
-    #print(word.text, word.shape_, word.is_alpha, word.is_stop)
-    print(word.text, word.pos_, word.tag_, word.dep_, word.lemma_)
 
-for word  in userquestion.ents:
-    print(word.text, word.label_)
+
+# for word  in userquestion:
+#     #print(word.text, word.shape_, word.is_alpha, word.is_stop)
+#     print(word.text, word.pos_, word.tag_, word.dep_, word.lemma_)
+#
+# for word  in userquestion.ents:
+#     print(word.text, word.label_)
 
 #print(spacy.explain('VBG'))
 #rint(displacy.render(userquestion, style='dep'))
 
-for token in userquestion.noun_chunks:
-    print(token.root.text, token.root.head.text)
+# for token in userquestion.noun_chunks:
+#     print(token.root.text, token.root.head.text)
+
+# class Parser:
+#     nlp = spacy.load('en_core_web_lg')
+#     userquestion = None
+#
+#     def __init__(self, text=None):
+#         if text == None:
+#             pass
+#         else:
+#             self.userquestion = nlp(text)
+
+
+
+
