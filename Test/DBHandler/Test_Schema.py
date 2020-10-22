7183840ff0f7e214c0bb1d3fb14d986ac7acbe9b
Barbara Böszöri <eboszori@gmail.com>
2020-10-22 19:36:02 +0200

    Testing and fixing MySQL Connections

--- Test/DBHandler/Test_Schema.py
+++ Test/DBHandler/Test_Schema.py
@@ -2,6 +2,15 @@ from Model.DBHandler.Schema import Schema
 import mysql.connector
 from mysql.connector import (connection)
 from mysql.connector import errorcode
+import pyodbc
+import pymssql
+
+# MS SQL connection
+# https://docs.microsoft.com/en-us/sql/connect/python/pyodbc/step-3-proof-of-concept-connecting-to-sql-using-pyodbc?view=sql-server-ver15
+
+# cnxn = pyodbc.connect(driver='{SQL Server Native Client 11.0}',
+#                         Server='127.0.0.1,1443,SDSTEST',
+#                         user='SDSReportUser', password='SDSreportpass#2016')
 
 # MySQL connection
 # https://dev.mysql.com/doc/connector-python/en/connector-python-example-connecting.html
