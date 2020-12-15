ab268be46f17554381336bf600fcc56484c86bff
Barbara Böszöri <eboszori@gmail.com>
2020-12-15 16:02:28 +0100

    Error handling

--- setup.py
+++ setup.py
@@ -1,20 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='szakdolgozat_IAPW0K',
-     version='1.0',
+    name='datawiz',
      author='Boszori, Barbara Eszter',
      packages=find_packages(),
      entry_points='''
          [gui_scripts]
-         szakdolgozat_IAPW0K=wiz.py
+         datawiz=main.wiz.py
      ''',
      install_requires=[
          'spacy',
          'en-core-web-sm',
          'pandas',
          'pandastable',
-         'mysql',
-         'tkinter'
+         'mysql'
      ]
 )
